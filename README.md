ac03e4c9c2d2493f4e73ad61d71e8b2f4b877be1
Yamamoto Tomoyuki <v25y2017@oita-u.ac.jp>
2025-06-08 03:57:34 +0000

    FaaSでWebAPIサーバ構築

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-To install dependencies:
-```sh
-bun install
+```txt
+npm install
+npm run dev
 ```
 
-To run:
-```sh
-bun run dev
+```txt
+npm run deploy
 ```
 
-open http://localhost:3000
+[For generating/synchronizing types based on your Worker configuration run](https://developers.cloudflare.com/workers/wrangler/commands/#types):
+
+```txt
+npm run cf-typegen
+```
+
+Pass the `CloudflareBindings` as generics when instantiation `Hono`:
+
+```ts
+// src/index.ts
+const app = new Hono<{ Bindings: CloudflareBindings }>()
+```
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,47 @@ import { zValidator } from '@hono/zod-validator'
 import { z, createRoute, OpenAPIHono} from '@hono/zod-openapi'
 import { swaggerUI } from '@hono/swagger-ui'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
 app.use(logger())
+
+app.openapi(
+  createRoute({
+    method: 'get',
+    path: '/api',
+    request: {
+      params: z.object({name: z.string().optional()})
+    },
+    responses:{
+      200: {
+        description: 'Respond a hello message',
+        content: {
+          'application/json': {schema:z.object({message: z.string()})}
+
+        }
+      }
+    }
+  }),
+  (c) => {
+    const query = c.req.query('name');
+    return c.json({message: `Hello ${query}!`})
+  }
+)
+
+app.doc('/doc', {
+  info:{
+    title: 'tokuron API',
+    version: 'v1'
+  },
+  openapi: '3.1.0',
+})
+
+app.get('/ui',
+  swaggerUI({
+    url: '/doc',
+  })
+)
+
 
 app.post('/api',
   zValidator('form', z.object({body: z.string()})),
@@ -22,11 +60,11 @@ app.get('/', (c) => {
 
 app.post('/api', (c) => c.text('POST /api'))
 
-app.get('/api', (c) => {
-  const query = c.req.query('name');
-  return c.json({message: `Hello ${query}!`})
-}
-)
+// app.get('/api', (c) => {
+//   const query = c.req.query('name');
+//   return c.json({message: `Hello ${query}!`})
+// }
+// )
 
 app.notFound((c) => c.text('Custom 404 Message', 404))
 
